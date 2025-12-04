2eea77e55cf96a212ced0c8a1991a0ebcc3b8f00
MyName <me@test.com>
2025-12-04 23:45:05 +0800

    修正嚴格模式錯誤

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import {
   Printer
 } from 'lucide-react';
 import { 
-  BarChart, 
   Bar, 
   XAxis, 
   YAxis, 
@@ -221,8 +220,7 @@ const MillionDollarGiftTab = ({ data, setData }: { data: GiftState, setData: (d:
   const phase2_Income = monthlyInvestIncomeSingle * 2; 
   const phase2_NetOut = phase2_LoanPmt - phase2_Income;
   const phase2_TotalCost = phase2_NetOut * 12 * loanTerm;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const projectTotalCost = phase1_TotalCost + phase2_TotalCost;
+  
   const standardTotalCost = targetAmount * 10000; 
   const standardMonthlySaving = standardTotalCost / (loanTerm * 2 * 12);
 
