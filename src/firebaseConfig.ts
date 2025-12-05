347beee211f8039618e55605596782a3f8d5e31f
MyName <me@test.com>
2025-12-05 23:18:53 +0800

    新增名片功能

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,21 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; // 新增這行
 
+// 您的 Firebase 連線設定
 const firebaseConfig = {
-  apiKey: "AIzaSyAqS6fhHQVyBNr1LCkCaQPyJ13Rkq7bfHA", 
+  apiKey: "AIzaSyAqs6fhHQVyBNr1LCkCaQPyJ13Rkq7bfHA", 
   authDomain: "grbt-f87fa.firebaseapp.com",
   projectId: "grbt-f87fa",
-  storageBucket: "grbt-f87fa.firebasestorage.app",
+  storageBucket: "grbt-f87fa.firebasestorage.app", // 這是存圖片的桶子
   messagingSenderId: "169700005946",
   appId: "1:169700005946:web:9b0722f31aa9fe7ad13d03",
   measurementId: "G-58N4KK9M5W"
 };
 
+// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
 
+// 匯出功能給 App.tsx 使用
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const storage = getStorage(app); // 新增這行：匯出 Storage 功能
 
 export default app;
